e426138eb1ef32fd8ce10fb9b26e088a20e68a73
azadk53 <mistazad@gmail.com>
2022-03-10 16:27:11 +0100

    reprt and liked pic fixed

--- js/app.js
+++ js/app.js
@@ -142,7 +142,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
-
+  document.getElementById("liked").innerHTML = '';
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
@@ -151,7 +151,7 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
-
+  document.getElementById("reported").innerHTML = '';
   const reportedPosts = getReportedPosts();
   reportedPosts.forEach((post) => {
     const div = createPost(post);
